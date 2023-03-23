325e7747e6e00b7df7f25f1c2f9e6f9003b4873a
Jonathan Mikalov <jmikalov@bu.edu>
2023-03-23 13:03:48 -0400

    add tentative comments

--- Graphical User Interface/taskwindow.py
+++ Graphical User Interface/taskwindow.py
@@ -1,5 +1,16 @@
 import tkinter as tk
+import time
 
+# def changecirc(canvas):
+#     i =  0
+#     for i in range(6):
+#         canvas.itemconfig(i, fill='yellow')
+#         if i > 0 and i != 5:
+#             canvas.itemconfig(i-1, fill='gray')
+#         if i == 6:
+#             i = -1
+        
+#         i  = i + 1
 
 def tWindow():
 
@@ -20,12 +31,16 @@ def tWindow():
     canvas = tk.Canvas(window, width=450, height=450)
     canvas.pack()
 
-    canvas.create_oval(25, 25, 75, 75, outline='black', width=2)
-    canvas.create_oval(100, 25, 150, 75, outline='black', width=2)
-    canvas.create_oval(175, 25, 225, 75, outline='black', width=2)
-    canvas.create_oval(25, 100, 75, 150, outline='black', width=2)
-    canvas.create_oval(100, 100, 150, 150, outline='black', width=2)
-    canvas.create_oval(175, 100, 225, 150, outline='black', width=2)
+    ovals = []
+
+    ovals.append(canvas.create_oval(25, 25, 75, 75, outline='black', width=2))
+    ovals.append(canvas.create_oval(100, 25, 150, 75, outline='black', width=2))
+    ovals.append(canvas.create_oval(175, 25, 225, 75, outline='black', width=2))
+    ovals.append(canvas.create_oval(25, 100, 75, 150, outline='black', width=2))
+    ovals.append(canvas.create_oval(100, 100, 150, 150, outline='black', width=2))
+    ovals.append(canvas.create_oval(175, 100, 225, 150, outline='black', width=2))
+
+
 
     canvas.scale("all", 0, 0, 2, 2)
     canvas.place(relx=0.45, rely=0.05, anchor='n')
@@ -35,3 +50,7 @@ def tWindow():
     canvas.create_text(100, 250, text="4", fill="black", font=('Helvetica 25 bold'))
     canvas.create_text(250, 250, text="5", fill="black", font=('Helvetica 25 bold'))
     canvas.create_text(400, 250, text="6", fill="black", font=('Helvetica 25 bold'))        
+    
+
+
+    window.mainloop()
